92a0cec7b1182d6572686aaedc9104f2d33c2855
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-06-07 17:04:44 +0100

    chore: bump

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,4 @@ name             'solr-schema'
 license          'MIT Licence'
 description      ''
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
